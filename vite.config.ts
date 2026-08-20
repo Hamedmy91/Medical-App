a98fc282c5f3d4ce06bc76cf8bb0a7f4ed2fbb1e
milan1990 <hamed.moyasseri2012@gmail.com>
2026-08-20 18:03:23 +0330

    manifest2

--- vite.config.ts
+++ vite.config.ts
@@ -13,88 +13,26 @@ export default defineConfig({
       registerType: "autoUpdate",
       injectRegister: "auto",
       manifest: {
-        name: "My App",
-        short_name: "App",
-        description: "A fast, reliable PWA.",
+        name: "Medical App",
+        short_name: "medical",
+        description:
+          "A progressive web app that delivers an amazing experience.",
         start_url: "/",
         scope: "/",
         display: "standalone",
-        orientation: "any",
+        theme_color: "#6366f1",
         background_color: "#ffffff",
-        theme_color: "#000000",
+        orientation: "any",
         icons: [
           {
-            src: "icons/icon-48x48.png",
-            sizes: "48x48",
-            type: "image/png",
-          },
-          {
-            src: "icons/icon-72x72.png",
-            sizes: "72x72",
-            type: "image/png",
-          },
-          {
-            src: "icons/icon-96x96.png",
-            sizes: "96x96",
-            type: "image/png",
-          },
-          {
-            src: "icons/icon-128x128.png",
-            sizes: "128x128",
-            type: "image/png",
-          },
-          {
-            src: "icons/icon-144x144.png",
-            sizes: "144x144",
-            type: "image/png",
-          },
-          {
-            src: "icons/icon-152x152.png",
-            sizes: "152x152",
-            type: "image/png",
-          },
-          {
-            src: "icons/icon-192x192.png",
+            src: "/icon-192x192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "icons/icon-256x256.png",
-            sizes: "256x256",
-            type: "image/png",
-          },
-          {
-            src: "icons/icon-384x384.png",
-            sizes: "384x384",
-            type: "image/png",
-          },
-          {
-            src: "icons/icon-512x512.png",
+            src: "/icon-512x512.png",
             sizes: "512x512",
             type: "image/png",
-          },
-        ],
-      },
-      workbox: {
-        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
-        runtimeCaching: [
-          {
-            urlPattern: /^https:\/\/dummyjson\.com\/products.*$/i,
-
-            handler: "NetworkFirst",
-
-            options: {
-              cacheName: "products-api-cache",
-
-              expiration: {
-                maxEntries: 20,
-                maxAgeSeconds: 60 * 60,
-              },
-
-              cacheableResponse: {
-                statuses: [0, 200],
-              },
-            },
           },
         ],
       },
